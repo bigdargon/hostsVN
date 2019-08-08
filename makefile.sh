de8777b9b32334c3120e10cdb5ea60d41be7a73f
hostsVN <10969626+bigdargon@users.noreply.github.com>
2019-08-08 16:10:03 +0700

    Update hosts

--- makefile.sh
+++ makefile.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+# make time stamp update
+TIME_STAMP=`date +'%d %b %Y %H:%M'`
+sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver.txt > tmp/title-adserver.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver-all.txt > tmp/title-adserver-all.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts-iOS.txt > tmp/title-hosts-iOS.tmp
+
+# create adserver files
+cat filters/list-adservers.txt | grep -v '!' | awk '{print $1}' >> tmp/list-adservers.tmp
+cat filters/list-adservers-all.txt | grep -v '!' | awk '{print $1}' >> tmp/list-adservers-all.tmp
+cat tmp/title-adserver.tmp tmp/list-adservers.tmp > filters/domain-adservers.txt
+cat tmp/title-adserver-all.tmp tmp/list-adservers.tmp tmp/list-adservers-all.tmp > filters/domain-adservers-all.txt
+
 # create files
 cat hosts | grep -v '#' | grep -v -e '^[[:space:]]*$' | awk '{print $2}' >> tmp/domain.txt
 cat hosts | grep -v '#' | grep -v -e '^[[:space:]]*$' | awk '{print "0 "$2}' >> tmp/hosts-iOS.tmp
 cat filters/domain-adservers.txt | grep -v '!' | awk '{print "||"$1"^"}' >> tmp/adservers-rule.tmp
 cat filters/domain-adservers-all.txt | grep -v '!' | awk '{print "||"$1"^"}' >> tmp/adservers-all-rule.tmp
 cat filters/domain-adservers-all.txt | grep -v '!' | awk '{print "*"$1" = 0.0.0.0"}' >> tmp/adservers-config.tmp
-
-# make time stamp update
-TIME_STAMP=`date +'%d %b %Y %H:%M'`
-sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver.txt > tmp/title-adserver.tmp
-sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver-all.txt > tmp/title-adserver-all.tmp
-sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts-iOS.txt > tmp/title-hosts-iOS.tmp
 
 # add to files
 cat tmp/title-hosts-iOS.tmp tmp/hosts-iOS.tmp > option/hosts-iOS
