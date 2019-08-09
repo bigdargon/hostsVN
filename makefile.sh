e9ac534269ee622e83ec74616eb4d60d606d1c35
hostsVN <10969626+bigdargon@users.noreply.github.com>
2019-08-09 11:32:32 +0700

    Update hosts

--- makefile.sh
+++ makefile.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # make time stamp update
-TIME_STAMP=`date +'%d %b %Y'`
+TIME_STAMP=`date +'%d %b %Y %H:%M'`
 sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver.txt > tmp/title-adserver.tmp
 sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-adserver-all.txt > tmp/title-adserver-all.tmp
 sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts.txt > tmp/title-hosts.tmp
-sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts-iOS.txt > tmp/title-hosts-iOS.tmp
 sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts-VN.txt > tmp/title-hosts-VN.tmp
+sed -e "s/_time_stamp_/$TIME_STAMP/g" tmp/title-hosts-iOS.txt > tmp/title-hosts-iOS.tmp
 
 # create adserver files
 cat source/list-adservers.txt | grep -v '!' | awk '{print $1}' >> tmp/list-adservers.tmp
@@ -22,9 +22,9 @@ cat filters/domain-adservers-all.txt | grep -v '!' | awk '{print "||"$1"^"}' >> 
 cat filters/domain-adservers-all.txt | grep -v '!' | awk '{print "*"$1" = 0.0.0.0"}' >> tmp/adservers-config.tmp
 
 # add to files
-cat tmp/title-hosts.tmp source/list-hosts.txt > hosts
+cat tmp/title-hosts.tmp source/list-hosts-group.txt source/list-hosts-VN-group.txt source/list-hosts-VN.txt source/list-hosts.txt > hosts
+cat tmp/title-hosts-VN.tmp source/list-hosts-VN-group.txt source/list-hosts-VN.txt > option/hosts-VN
 cat tmp/title-hosts-iOS.tmp tmp/hosts-iOS.tmp > option/hosts-iOS
-cat tmp/title-hosts-VN.tmp source/list-hosts-VN.txt > option/hosts-VN
 cat tmp/title-adserver.tmp tmp/adservers-rule.tmp > filters/adservers.txt
 cat tmp/title-adserver-all.tmp tmp/adservers-all-rule.tmp > filters/adservers-all.txt
 cat tmp/title-config-surge.txt tmp/adservers-config.tmp > option/hostsVN.conf
