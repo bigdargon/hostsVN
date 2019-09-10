8a287b753580a8c4b4b837a479a01f25b0ce9b59
hostsVN <10969626+bigdargon@users.noreply.github.com>
2019-09-10 12:13:50 +0700

    Update hosts

--- makefile.sh
+++ makefile.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
+echo "Making titles..."
 # make time stamp & count blocked
 TIME_STAMP=`date +'%d %b %Y %H:%M'`
-COUNT_DOMAIN=$(cat source/list-hosts-group.txt source/list-hosts-group-VN.txt source/list-hosts-VN.txt source/list-hosts.txt | grep "0.0.0.0" | wc -l)
-DOMAIN=$(printf "%'.f\n" "$COUNT_DOMAIN")
-COUNT_DOMAIN_VN=$(cat source/list-hosts-group-VN.txt source/list-hosts-VN.txt | grep "0.0.0.0" | wc -l)
-DOMAIN_VN=$(printf "%'.f\n" "$COUNT_DOMAIN_VN")
-COUNT_RULE=$(cat source/list-adservers.txt source/list-adservers-all.txt | grep -v '!' | wc -l)
-RULE=$(printf "%'.f\n" "$COUNT_RULE")
-COUNT_RULE_VN=$(cat source/list-adservers.txt | grep -v '!' | wc -l)
-RULE_VN=$(printf "%'.f\n" "$COUNT_RULE_VN")
+DOMAIN=$(printf "%'.f\n" $(cat source/list-hosts-group.txt source/list-hosts-group-VN.txt source/list-hosts-VN.txt source/list-hosts.txt | grep "0.0.0.0" | wc -l))
+DOMAIN_VN=$(printf "%'.f\n" $(cat source/list-hosts-group-VN.txt source/list-hosts-VN.txt | grep "0.0.0.0" | wc -l))
+RULE=$(printf "%'.f\n" $(cat source/list-adservers.txt source/list-adservers-all.txt | grep -v '!' | wc -l))
+RULE_VN=$(printf "%'.f\n" $(cat source/list-adservers.txt | grep -v '!' | wc -l))
 
 # update titles
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_rule_vn_/$RULE_VN/g" tmp/title-adserver.txt > tmp/title-adserver.tmp
@@ -18,6 +15,7 @@ sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_domain_/$DOMAIN/g" tmp/title-hosts.
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_domain_/$DOMAIN/g" tmp/title-hosts-iOS.txt > tmp/title-hosts-iOS.tmp
 sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_domain_vn_/$DOMAIN_VN/g" tmp/title-hosts-VN.txt > tmp/title-hosts-VN.tmp
 
+echo "Creating hosts file..."
 # create hosts files
 cat tmp/title-hosts.tmp source/list-hosts-group.txt source/list-hosts-group-VN.txt source/list-hosts-VN.txt source/list-hosts.txt > hosts
 cat tmp/title-hosts-VN.tmp source/list-hosts-group-VN.txt source/list-hosts-VN.txt > option/hosts-VN
@@ -30,21 +28,29 @@ cat tmp/title-hosts-iOS.tmp tmp/hosts-iOS.tmp > option/hosts-iOS
 cat hosts | grep -v '#' | grep -v -e '^[[:space:]]*$' | awk '{print $2}' >> tmp/domain.txt
 mv tmp/domain.txt option/
 
+echo "Creating temp file..."
 # create adserver files
 cat source/list-adservers.txt | grep -v '!' | awk '{print $1}' >> tmp/list-adservers.tmp
 cat source/list-adservers-all.txt | grep -v '!' | awk '{print $1}' >> tmp/list-adservers-all.tmp
-cat tmp/title-adserver-all.tmp tmp/list-adservers.tmp tmp/list-adservers-all.tmp > filters/domain-adservers-all.txt
 
 # create rule & config files
 cat tmp/list-adservers.tmp | awk '{print "||"$1"^"}' >> tmp/adservers-rule.tmp
 cat tmp/list-adservers.tmp tmp/list-adservers-all.tmp | awk '{print "||"$1"^"}' >> tmp/adservers-all-rule.tmp
 cat tmp/list-adservers.tmp tmp/list-adservers-all.tmp | awk '{print "*"$1" = 0.0.0.0"}' >> tmp/adservers-config.tmp
 
+echo "Adding to file..."
 # add to files
 cat tmp/title-adserver.tmp tmp/adservers-rule.tmp > filters/adservers.txt
 cat tmp/title-adserver-all.tmp tmp/adservers-all-rule.tmp > filters/adservers-all.txt
+cat tmp/title-adserver-all.tmp tmp/list-adservers.tmp tmp/list-adservers-all.tmp > filters/domain-adservers-all.txt
 cat tmp/title-config-surge.txt tmp/adservers-config.tmp > option/hostsVN.conf
 cat tmp/title-config-quantumult.txt tmp/adservers-config.tmp > option/hostsVN-quantumult.conf
 
 # remove tmp file
 rm -rf tmp/*.tmp
+
+# check duplicate
+echo "Checking duplicate..."
+sort option/domain.txt | uniq -d
+sort filters/adservers-all.txt | uniq -d
+read -p "Completed! Press enter to close"
