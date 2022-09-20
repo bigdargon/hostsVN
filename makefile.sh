a6967e54979f3b77fd8403def5d9c23586354431
BigDargon <10969626+bigdargon@users.noreply.github.com>
2022-09-20 23:41:31 +0700

    Update script

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+echo "Preparing files..."
+# convert hosts to filters
+cat source/hosts.txt | grep "0.0.0.0" | awk '{print $2}' > source/adserver-all.tmp
+sed -i "s/www\.//g" source/adserver-all.tmp
+sort -u -o source/adserver-all.tmp source/adserver-all.tmp
+cat source/hosts-VN.txt | grep "0.0.0.0" | awk '{print $2}' > source/adserver.tmp
+sed -i "s/www\.//g" source/adserver.tmp
+sort -u -o source/adserver.tmp source/adserver.tmp
+
 echo "Making titles..."
 # make time stamp & count blocked
 TIME_STAMP=$(date +'%d %b %Y %H:%M')
@@ -7,8 +16,8 @@ VERSION=$(date +'%y%m%d%H%M')
 LC_NUMERIC="en_US.UTF-8"
 DOMAIN=$(printf "%'.3d\n" $(cat source/hosts-group.txt source/hosts-VN-group.txt source/hosts-VN.txt source/hosts.txt source/hosts-extra.txt | grep "0.0.0.0" | wc -l))
 DOMAIN_VN=$(printf "%'.3d\n" $(cat source/hosts-VN-group.txt source/hosts-VN.txt | grep "0.0.0.0" | wc -l))
-RULE=$(printf "%'.3d\n" $(cat source/adservers.txt source/adservers-all.txt source/adservers-extra.txt source/exceptions.txt | grep -v '!' | wc -l))
-RULE_VN=$(printf "%'.3d\n" $(cat source/adservers.txt | grep -v '!' | wc -l))
+RULE=$(printf "%'.3d\n" $(cat source/adservers.txt source/adservers-all.txt source/adserver-all.tmp source/adservers-extra.txt source/exceptions.txt | grep -v '!' | wc -l))
+RULE_VN=$(printf "%'.3d\n" $(cat source/adservers.txt source/adserver.tmp | grep -v '!' | wc -l))
 HOSTNAME=$(cat source/config-hostname.txt)
 
 # update titles
@@ -37,10 +46,10 @@ cat hosts | grep -v '#' | grep -v -e '^[[:space:]]*$' | awk '{print $2}' > optio
 
 echo "Creating adserver file..."
 # create temp adserver files
-cat source/adservers.txt | grep -v '!' | awk '{print $1}' >> tmp/adservers.tmp
-cat source/adservers-all.txt | grep -v '!' |awk '{print $1}' >> tmp/adservers-all.tmp
-cat source/adservers-extra.txt | grep -v '!' |awk '{print $1}' >> tmp/adservers-extra.tmp
-cat source/exceptions.txt | grep -v '!' |awk '{print $1}' >> tmp/exceptions.tmp
+cat source/adservers.txt source/adserver.tmp | grep -v '!' | awk '{print $1}' >> tmp/adservers.tmp
+cat source/adservers-all.txt source/adserver-all.tmp | grep -v '!' | awk '{print $1}' >> tmp/adservers-all.tmp
+cat source/adservers-extra.txt | grep -v '!' | awk '{print $1}' >> tmp/adservers-extra.tmp
+cat source/exceptions.txt | grep -v '!' | awk '{print $1}' >> tmp/exceptions.tmp
 
 cat tmp/adservers.tmp | awk '{print "||"$1"^"}' >> tmp/adservers-rule.tmp
 cat tmp/adservers.tmp tmp/adservers-all.tmp tmp/adservers-extra.tmp | awk '{print "||"$1"^"}' >> tmp/adservers-all-rule.tmp
@@ -90,6 +99,7 @@ cat source/OTA.txt | grep -v '!' | awk '{print "DOMAIN-SUFFIX,"$1}' > option/hos
 
 # remove tmp file
 rm -rf tmp/*.tmp
+rm -rf source/*.tmp
 
 # check duplicate
 echo "Checking duplicate..."
